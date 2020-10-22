490fc32f221ebcaaa45acbf6a42b56d28c7c90f7
rtp99 <ryan.t.perkins13@gmail.com>
2020-10-22 15:05:22 -0700

    Updated Parser to handle negatives properly

--- InfixParser.cpp
+++ InfixParser.cpp
@@ -1,9 +1,9 @@
 #include "InfixParser.h"
 
 std::vector<std::string> InfixParser::parseString(std::string infix) {
-    std::stack<char> proc_stack;           // stack by which we process the input
+    std::stack<char> proc_stack;            // stack by which we process the input
     proc_stack.push('\0');               // '\0' is our end-of-stack marker
-    std::vector<std::string> output_vec;   // output vector to hold final data
+    std::vector<std::string> output_vec;    // output vector to hold final data
     std::string::iterator input_itr;
 
     std::string dig_acc = "";   // digit accumulator, used for combining individual digits into full numbers
@@ -11,7 +11,9 @@ std::vector<std::string> InfixParser::parseString(std::string infix) {
     for (input_itr = infix.begin(); input_itr != infix.end(); input_itr++) {
         // ignore whitespace
         if(*input_itr == ' ') {}
-        // if current character is a digit, collect all digits in final number, and push to output vector
+        // ignore equals signs at the end of equations
+        else if (*input_itr == '=' && *input_itr == *--infix.end()) {}
+            // if current character is a digit, collect all digits in final number, and push to output vector
         else if (isdigit(*input_itr)) {
             while (isdigit(*input_itr)) {
                 dig_acc.push_back(*input_itr);
@@ -48,16 +50,15 @@ std::vector<std::string> InfixParser::parseString(std::string infix) {
                 proc_stack.pop();
             }
         } else if (isOperator(*input_itr)) {
-            if (*input_itr == '-' && dig_acc != "-") {
-                // if the previous character was interpreted as a negative, this is a minus sign
-                dig_acc.push_back(*input_itr);
-            } else {
+            if (*input_itr != '-' || (*input_itr == '-' && dig_acc == "-")) {
                 while (proc_stack.top() != '\0' && getPrecedence(*input_itr) <= getPrecedence(proc_stack.top())) {
                     char top = proc_stack.top();
                     proc_stack.pop();
                     output_vec.emplace_back(1, top);
                 }
                 proc_stack.push(*input_itr);
+            } else {
+                dig_acc.push_back('-');
             }
         } else {
             // TODO:THROW EXCEPTION
