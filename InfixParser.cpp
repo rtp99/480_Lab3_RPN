b78e1a6ea545a1ee742ddb3aee79edf0ed239b1a
rtp99 <ryan.t.perkins13@gmail.com>
2020-10-23 17:12:58 -0700

    Updated Comments and verified all tests are good

--- InfixParser.cpp
+++ InfixParser.cpp
@@ -1,5 +1,6 @@
 #include "InfixParser.h"
 
+// shunting-yard algorithm that turns an infix-formatted string into a vector of strings, with contents in postfix order
 std::vector<std::string> InfixParser::parseString(std::string infix) {
     std::stack<std::string> proc_stack;     // stack by which we process the input
     proc_stack.push("\0");               // '\0' is our end-of-stack marker
@@ -12,12 +13,12 @@ std::vector<std::string> InfixParser::parseString(std::string infix) {
     for (input_itr = infix.begin(); input_itr != infix.end(); input_itr++) {
         // ignore whitespace
         if (*input_itr == ' ') {}
-            // ignore equals signs at the end of equations
+        // ignore equals signs at the end of equations
         else if (*input_itr == '=' && *input_itr == *--infix.end()) {}
-            // if current character is a digit, collect all digits in final number, and push to output vector
-            // handles decimal places
+        // if current character is a digit, collect all digits in final number, and push to output vector
         else if (isdigit(*input_itr) || *input_itr == '.') {
             int decimal_counter = 0;
+            // handle decimal places, and prevent multiple decimals
             while (isdigit(*input_itr) || *input_itr == '.') {
                 if (*input_itr == '.')
                     decimal_counter++;
@@ -33,7 +34,7 @@ std::vector<std::string> InfixParser::parseString(std::string infix) {
             input_itr--;
             neg_flag = false;
         }
-            // if current character is a letter, collect all letters to produce final function name
+        // if current character is a letter, collect all letters to produce final function name
         else if (isalpha(*input_itr)) {
             while (isalpha(*input_itr)) {
                 func_acc.push_back(*input_itr);
@@ -44,16 +45,19 @@ std::vector<std::string> InfixParser::parseString(std::string infix) {
             input_itr--;
             neg_flag = false;
         }
-            //if current character is an open paren, push to stack
+        // if current character is an open paren, push to stack
         else if (*input_itr == '(') {
             proc_stack.push(std::string(1, *input_itr));
             neg_flag = true;
         } else if (*input_itr == ')') {
+            // if current character is a close paren, push the operators between the parens to the stack
             while (proc_stack.top() != "\0" && proc_stack.top() != "(") {
                 std::string top = proc_stack.top();
                 proc_stack.pop();
                 output_vec.emplace_back(top);
             }
+            // pull the open paren off the stack
+            // if it doesn't exist, this is a syntax error
             if (proc_stack.top() == "(") {
                 proc_stack.pop();
             } else {
@@ -62,10 +66,12 @@ std::vector<std::string> InfixParser::parseString(std::string infix) {
             }
             neg_flag = false;
         } else if (isOperator(*input_itr)) {
+            // if this '-' sign is a negative, replace it with this unary function and push to stack
             if (neg_flag && *input_itr == '-') {
                 proc_stack.push("neg");
                 neg_flag = true;
             } else {
+                // pop lower precedence operators off the stack onto output
                 while (proc_stack.top() != "\0" && getPrecedence(*input_itr) <= getPrecedence(proc_stack.top()[0])) {
                     std::string top = proc_stack.top();
                     proc_stack.pop();
@@ -75,12 +81,13 @@ std::vector<std::string> InfixParser::parseString(std::string infix) {
                 proc_stack.push(std::string(1, *input_itr));
             }
         } else {
-            // TODO:THROW EXCEPTION
+            // Unexcpected character, return an error
             std::string error = "invalid character ";
             error.push_back(*input_itr);
             throw error;
         }
     }
+    // pop remaining operators to the output
     while (proc_stack.top() != "\0") {
         std::string top = proc_stack.top();
         proc_stack.pop();
@@ -89,10 +96,12 @@ std::vector<std::string> InfixParser::parseString(std::string infix) {
     return output_vec;
 }
 
+// simply returns if a character is a basic, non-function operator
 bool InfixParser::isOperator(char input) {
     return (input == '*' || input == '/' || input == '+' || input == '-' || input == '^');
 }
 
+// returns the precedence of the operator
 int InfixParser::getPrecedence(char input) {
     if (isalpha(input))
         return 4;
--- RPNSolver.cpp
+++ RPNSolver.cpp
@@ -1,14 +1,16 @@
 #include "RPNSolver.h"
 
+//processes a vector of strings in postfix order and produces a result
 double RPNSolver::solveRPN(std::vector<std::string> RPN) {
     std::stack<float> op_stack;
     op_stack.push(0);
     std::vector<std::string>::iterator itr = RPN.begin();
     double op1, op2;
+
     for (itr = RPN.begin(); itr != RPN.end(); itr++) {
         // we only have to check if the last character in the string is a digit
         // the parser automatically separates digits from non digits
-        // because negative signs exist, we check the last digit
+        // because negative signs exist, we check the last digit available
         if (isdigit((*itr).back())) {
             op_stack.push(std::stod(*itr));
         } else if (*itr == "+") {
@@ -29,12 +31,12 @@ double RPNSolver::solveRPN(std::vector<std::string> RPN) {
             op1 = op_stack.top();
             op_stack.pop();
             op_stack.push(op1 * op2);
-
         } else if (*itr == "/") {
             op2 = op_stack.top();
             op_stack.pop();
             op1 = op_stack.top();
             op_stack.pop();
+
             if (op1 == 0) {
                 std::string error = "incorrect syntax, division by 0";
                 throw error;
@@ -43,6 +45,7 @@ double RPNSolver::solveRPN(std::vector<std::string> RPN) {
                 std::string error = "incorrect syntax, dividing 0";
                 throw error;
             }
+
             op_stack.push(op1 / op2);
         } else if (*itr == "^") {
             op2 = op_stack.top();
@@ -65,6 +68,7 @@ double RPNSolver::solveRPN(std::vector<std::string> RPN) {
         } else if (*itr == "cot") {
             op1 = op_stack.top();
             op_stack.pop();
+            // cotangent does not have a math library function, this is the most stable way to perform cotangent
             op_stack.push(cos(op1) / sin(op1));
         } else if (*itr == "ln") {
             op1 = op_stack.top();
@@ -90,10 +94,12 @@ double RPNSolver::solveRPN(std::vector<std::string> RPN) {
         }
 
     }
+    // Prevent infinite return values
     if (op_stack.top() == FP_INFINITE) {
         std::string error = "incorrect syntax, returns infinity";
         throw error;
     }
+    // Prevent NaN return values
     if (op_stack.top() == FP_NAN) {
         std::string error = "incorrect syntax, division by 0";
         throw error;
